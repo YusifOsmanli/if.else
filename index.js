35f12c79c5c81bdd02143170871c3165f1325e03
YusifOsmanli <tu6xkjyle@code.edu.com>
2023-10-17 00:06:40 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,153 @@
+//task1
+//
+//
+// var ay = parseInt(prompt("ayi daxil edin"))
+// switch(ay){
+//     case 1:
+//         alert("Yanvar");
+//         break;
+//     case 2:
+//         alert("Fevral");
+//         break;
+//     case 3:
+//         alert("Mart") 
+//         break;
+//     case 4:
+//         alert("Aprel")
+//         break;
+//     case 5:
+//         alert("May")
+//         break;
+//     case 6:
+//         alert("Iyun")
+//         break;
+//     case 7:
+//         alert("Iyul")
+//         break;
+//     case 8:
+//         alert("Avqust")
+//         break;
+//     case 9:
+//         alert("Sentaybr")
+//         break;
+//     case 10:
+//         alert("Oktaybr")
+//         break;
+//     case 11:
+//         alert("Noyabr")
+//         break;
+//     case 12:
+//         alert("Dekabr")
+//         break;
+//     default:
+//         alert;("hello world")    
+// }
+
+
+//task2
+
+// var num = parseInt(prompt("reqem daxil edin"))
+
+// if(num>=0){
+//      if(num%3===0 && num%5===0){
+//         alert("Bu deyer 3 ve 5 bolunur")
+//      }else{
+//         alert("Bu deyer 3 ve 5 bolunmur")
+//      }
+// }else[
+//     alert("Musbet deyer daxil edin")
+// ]
+
+
+//task3
+
+// var num = parseInt(prompt("reqem daxil edin"))
+// if(num>=0){
+//        if(num%2===0){
+//         alert("Bolundu")
+//        }else{
+//         alert("1")
+//        }
+// }else{
+//     alert("Musbet deyer daxil edin")
+// }
+
+//task4
+// var num = parseInt(prompt("reqem daxil edin"))
+//   if(num>=0){
+//        alert("Musbet")
+//   }else{
+//     alert("Menfi")
+//   }
+
+//task5
+// var num = parseInt(prompt("reqem daxil edin"))
+//    if(num>=0){
+//        if(num>0 && num<100){
+//            alert("Uygundur")
+//        }else if(num>100 || num==0){
+//         alert("Uygun deil")
+//        }
+//    }else{
+//     alert("Musbet deyer yazin")
+//    }
+
+//task7
+// var num = parseInt(prompt("Balinizi daxil edin"))
+// if(num>=0 && num<=100){
+//         if(num>=90){
+//             alert("A")
+//         }if(num>=80 && num<90){
+//             alert("B")
+//         }if(num>=70 && num<80){
+//             alert("C")
+//         }
+//         if(num>=60 && num<70){
+//             alert("D")
+//         }if(num<60){
+//             alert("F")
+//         }
+// }else{
+//     alert("Duzgun bal yazin")
+// }
+
+//task8
+// var num = parseInt(prompt("Yashinizi daxil edin"))
+// if(num>0 && num<125){
+//     if(num<18){
+//         alert("Yeniyetme")
+//     }if(num>=18 && num<65){
+//         alert("Yetkin")
+//     }if(num>=65 && num<125){
+//         alert("Yashli")
+//     }
+// }else{
+//     alert("Duzgun yash daxil edin")
+// }
+
+//task9
+// var num = parseInt(prompt("Saati daxil edin"))
+// if(num>=0 && num<=24){
+//      if(num>0 && num<12){
+//         alert("Sabahiniz xeyir")
+//      }if(num>=12 && num<18){
+//         alert("Gunortaviz xeyir")
+//      }if(num>=18 && num<=24 ){
+//         alert("Axshamiviz xeyir")
+//      }
+// }else{
+//     alert("Duzgun vaxt daxil edin")
+// }
+
+//task10
+// var x = parseInt(prompt("Eded daxil edin"))
+// var y = parseInt(prompt("Eded daxil edin"))
+// if(x>0 && y<0){
+//     alert(4*x+2-y+4)
+// }
+// else if (x>0 && y===0){
+//     alert(2*x-y+3)
+// }
+// else if(x<0 && y>0){
+//     alert(3*x+4*y+3)
+// }
